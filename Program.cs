cb945708274aaedeb9e2dd72d75c7109c4851cfa
Dmitriy Filippov <dmitriy.filippov.reid@gmail.com>
2025-04-03 11:55:00 +0500

    add Stopwatch

--- Program.cs
+++ Program.cs
@@ -1,52 +1,127 @@
-﻿namespace HomeWork44
+﻿using System.Diagnostics;
+
+namespace HomeWork44
 {
     internal class Program
     {
+        static bool ShowSeries;
+
         static void FibonacciCicle(uint N)
         {
             if (N < 2)
             {
-                Console.WriteLine($"{N,2:d} ={N,5:d}");
+                if (ShowSeries)
+                    Console.WriteLine($"{N,2:d} ={N,5:d}");
             }
             else
             {
-                int f;
+                int f = 2;
                 int n_1 = 1;
                 int n_2 = 0;
-                Console.WriteLine($"{n_2,2:d} ={n_2,5:d}");
-                Console.WriteLine($"{n_1,2:d} ={n_1,5:d}");
+                if (ShowSeries)
+                {
+                    Console.WriteLine($"{n_2,2:d} ={n_2,5:d}");
+                    Console.WriteLine($"{n_1,2:d} ={n_1,5:d}");
+                }
                 for (int i = 1; i < N; i++)
                 {
-                    f = n_1 + n_2;
-                    n_2 = n_1;
-                    n_1 = f;
-                    Console.WriteLine($"{i+1,2:d} ={f,5:d}");
+                    (f, n_2, n_1) = (n_1 + n_2, n_1, n_1 + n_2);
+                    //f = n_1 + n_2;
+                    //n_2 = n_1;
+                    //n_1 = f;
+                    if (ShowSeries)
+                        Console.WriteLine($"{i+1,2:d} ={f,5:d}");
                 }
             }
         }
 
-        static uint FibonacciRecource(uint N, bool ToConsole)
+        static uint FibonacciRecursively(uint N, bool ToConsole)
         {
             if (N < 2)
             {
-                if (ToConsole)
+                if (ToConsole && ShowSeries)
                     Console.WriteLine($"{N,2:d} ={N,5:d}");
                 return N;
             }
-            uint f = FibonacciRecource(N - 1, (true && ToConsole)) + FibonacciRecource(N - 2, false);
-            if (ToConsole)
+            uint f = FibonacciRecursively(N - 1, (true && ToConsole)) + FibonacciRecursively(N - 2, false);
+            if (ToConsole && ShowSeries)
               Console.WriteLine($"{N,2:d} ={f,5:d}");
             return f;
         }
 
         static void Main(string[] args)
         {
-            Console.WriteLine($"FibonacciCicle");
-            FibonacciCicle(20);
-            Console.WriteLine($"");
-            Console.WriteLine($"FibonacciRecource");
-            Console.WriteLine($"{0,2:d} ={0,5:d}");
-            FibonacciRecource(20, true);
+            ShowSeries = true;
+
+            for (uint i = 5; i <= 20; i = i * 2)
+            //for (uint i = 20; i >= 5; i = i / 2)
+            //uint i = 20;
+            {
+                var sw = new Stopwatch();
+                sw.Start();
+                if (ShowSeries)
+                    Console.WriteLine($"FibonacciInCicle");
+                FibonacciCicle(i);
+                sw.Stop();
+                var elapsed_Cicle = sw.Elapsed;
+
+                Console.WriteLine($"");
+
+                sw.Restart();
+                if (ShowSeries)
+                {
+                    Console.WriteLine($"FibonacciRecursively");
+                    Console.WriteLine($"{0,2:d} ={0,5:d}");
+                }
+                FibonacciRecursively(i, true);
+                sw.Stop();
+                var elapsed_Recursively = sw.Elapsed;
+
+                Console.WriteLine($"");
+                Console.WriteLine($"In Cicle {i,2:d}    ={elapsed_Cicle}");
+                Console.WriteLine($"Recursively {i,2:d} ={elapsed_Recursively}");
+
+            }
         }
     }
+
+    /*
+    Отдельно:
+
+    In Cicle  5    =00:00:00.0004154
+    Recursively  5 =00:00:00.0002393
+
+    In Cicle 10    =00:00:00.0003860
+    Recursively 10 =00:00:00.0002348
+
+    In Cicle 20    =00:00:00.0004409
+    Recursively 20 =00:00:00.0005761 
+    
+    
+    С кешированием по возрастанию:
+    
+    In Cicle  5    =00:00:00.0003783
+    Recursively  5 =00:00:00.0002170
+
+    In Cicle 10    =00:00:00.0000006
+    Recursively 10 =00:00:00.0000025
+
+    In Cicle 20    =00:00:00.0000002
+    Recursively 20 =00:00:00.0001450        
+
+
+    С кешированием по убыванию:
+    
+    In Cicle 20    =00:00:00.0004164
+    Recursively 20 =00:00:00.0004473
+
+    In Cicle 10    =00:00:00.0000005
+    Recursively 10 =00:00:00.0000021
+
+    In Cicle  5    =00:00:00.0000002
+    Recursively  5 =00:00:00.0000005        
+
+    */
+
+
 }
